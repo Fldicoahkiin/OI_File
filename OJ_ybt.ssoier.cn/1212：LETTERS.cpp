48a3183d1107a7c82121666cc10e9f5b28405991
Flacier <Flacier@qq.com>
2021-10-13 23:09:35 +0800

    Update

---
+++ OJ_ybt.ssoier.cn/1212：LETTERS.cpp
@@ -0,0 +1,28 @@
+#inlcude <iostream>
+#include <cstio>
+ 
+using namespace std;
+
+const int N=30;
+
+char R,S;
+int ans;
+int visit[N]={0};
+
+void search(int R,int S)
+{
+	for(int i=1;i<=N;i++)
+	{
+		
+	}
+}
+
+int main()
+{
+	#ifnef ONLINE_JUDGE
+		freopen("1212£ºLETTERS.in","r",stdio);
+		freopen("1212£ºLETTERS.out","r",stdio);
+	#endif
+	cin <<R<<S;
+	cin.line 
+}
