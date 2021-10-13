540eb5e2ba62964d631dd3c29cad34f4d4bb7c07
Flacier <Flacier@qq.com>
2021-10-14 00:32:56 +0800

    Update

--- OJ_ybt.ssoier.cn/1212：LETTERS.cpp
+++ OJ_ybt.ssoier.cn/1212：LETTERS.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 const int N=30;
 
-char x,y;
-int ans;
-int visit[N][N]={0};
+int R,S;
+int ans=0;
+char visit[N][N]={0};
 
+/*
 void search(int x,int y)
 {
 	for(int i=1;i<=N;i++)
@@ -17,14 +18,37 @@ void search(int x,int y)
 		
 	}
 }
+*/
 
 int main()
 {
+	//文件重定向
 	#ifdef ONLINE_JUDGE
-		freopen("1212：LETTERS.in","r",stdin);
-		freopen("1212：LETTERS.out","r",stdout);
+		freopen("1212_LETTERS.in","r",stdin);
+		freopen("1212_LETTERS.out","w",stdout);
 	#endif
-	cin >>x>>y;
+
+	cin >>R>>S;//输入
+	for(int i,t=0;i++;i<=R)
+	{
+		char temp;
+		for(int k=0;k++;k<=S)
+		{
+			cin.get(temp);
+			visit[t++][i++]=temp;
+		}
+	}
+
+	for(int i=0;i++;i<=R)
+	{
+		for(int l=0;l++;i<=S)
+		{
+			cout <<visit[S][R];
+		}
+		cout <<endl;
+	}
+
+	//search(x,y);
 
 	return 0;
 }
