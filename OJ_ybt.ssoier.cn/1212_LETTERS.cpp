52fb51d71347b26ecff02d9488cbba33709e65af
Flacier <Flacier@qq.com>
2021-11-01 23:22:10 +0800

    Update LETTERS and ReName

--- OJ_ybt.ssoier.cn/1212_LETTERS.cpp
+++ OJ_ybt.ssoier.cn/1212_LETTERS.cpp
@@ -12,7 +12,7 @@ char map[N][N];
 int lettervisit[N];//1表示访问过,0表示未访问
 int visit[N][N]; //1表示访问过,0表示未访问
 
-int t=1,ans=0;
+int t=1,ans;
 
 int direct[5][2]={//direct[k][0]表示上下变化，direct[k][1]表示左右变化
 	{0,0},{-1,0},{1,0},{0,-1},{0,1}//分别对应原地，上，下，左，右
@@ -49,24 +49,20 @@ void printvisit()
 void dfs(int y,int x)
 {
 	ans=max(ans,t);
-	cout <<"ans="<<ans<<endl;
-	cout <<"t="<<t<<endl;
-
 	int tempx,tempy;
 
 	for(int k=1;k++;k<=4)
 	{
 		tempy=y+direct[k][0];
 		tempx=x+direct[k][1];
-		
-		printvisit();
-
 		if(tempy>=1 && tempx>=1 && tempy<=R && tempx<=S && lettervisit[int(map[tempy][tempx])]==0 && visit[tempy][tempx]==0)
 		{
-			lettervisit[int(map[tempy][tempx])]=visit[tempy][tempx]=1;
+			lettervisit[int(map[tempy][tempx])]=1;
 			t++;
+			visit[tempy][tempx]=t;
+			printvisit();
 			dfs(tempy,tempx);
-			lettervisit[int(map[tempy][tempx])]=visit[tempy][tempx]=0;printvisit();
+			lettervisit[int(map[tempy][tempx])]=visit[tempy][tempx]=0;
 		}
 	}
 }
@@ -85,13 +81,12 @@ int main()
 			cin>>map[i][j];
 		}
 	}
+	lettervisit[int(map[1][1])]=visit[1][1]=1;
 	printmap();
-
-	lettervisit[map[1][1]]=visit[1][1]=1;
-
+	
 	dfs(1,1);
-	
+
 	cout <<ans<<endl;
-	
+
 	return 0;
 }
