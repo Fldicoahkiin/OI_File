9b2582a3cdcbb47ccb6d458bdb398a98af2891a1
Flacier <Flacier@qq.com>
2021-11-17 23:57:06 +0800

    Debug fix

--- OJ/ybt.ssoier.cn/1320_均分纸牌_Noip2002_.cpp
+++ OJ/ybt.ssoier.cn/1320_均分纸牌_Noip2002_.cpp
@@ -14,7 +14,7 @@ int main()
 //	freopen("1320.out", "w" , stdout); 
 
 	cin >>n;
-	for(int i=1;i<=n;i++)
+	for(int i=1;i<n;i++)
 	{
 		cin >>a[i]; 
 		sum += a[i];
@@ -23,17 +23,20 @@ int main()
 	average=sum/n;
 
 //copy
-	int i = 1, j = n;
-	while (a[i] == 0 && i < n) i++;
-	while (a[j] == 0 && j > 1) j--;
-	while (i < j)
+	for(int i=1;i<n;i++)
 	{
-		a[i + 1] += a[i];
-		a[i] = 0; t++;
-		i++; while (a[i] == 0 && i < j) i++;
-
+		if(a[i]!=0)
+		{
+			a[i+1]=a[i]+a[i+1];
+			a[i]==0;
+			t++;
+		}
+		else
+		{
+			continue;
+		}
 	}
-	
+
 	cout <<t;
 	return 0;
 }
