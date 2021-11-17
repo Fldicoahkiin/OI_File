64ad2b0a7818810cfbcc3fee7efbf32d4a035291
Flacier <Flacier@qq.com>
2021-11-17 23:44:10 +0800

    Update copy

--- OJ/信奥赛一本通_ybt.ssoier.cn/1320_均分纸牌_Noip2002_.cpp
+++ OJ/信奥赛一本通_ybt.ssoier.cn/1320_均分纸牌_Noip2002_.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int n;
-int a[101];
+int a[102];
 int sum=0,average;
 int t=0;
 
 int main()
 {
-//	freopen("1320.in", "r" , stdin); 
+	freopen("1320.in", "r" , stdin); 
 //	freopen("1320.out", "w" , stdout); 
 
 	cin >>n;
@@ -19,21 +19,21 @@ int main()
 		cin >>a[i]; 
 		sum += a[i];
 	}
+
 	average=sum/n;
 
-	for(int i=1;i<=n;i++)
+//copy
+	int i = 1, j = n;
+	while (a[i] == 0 && i < n) i++;
+	while (a[j] == 0 && j > 1) j--;
+	while (i < j)
 	{
-		a[i]-=average;
-		if(a[i]<0)
-		{
-			t++;
-		}
+		a[i + 1] += a[i];
+		a[i] = 0; t++;
+		i++; while (a[i] == 0 && i < j) i++;
+
 	}
 	
-/*	for(int i=1;i<=n;i++)
-	{
-		cout <<a[i]<<endl;
-	}*/
 	cout <<t;
 	return 0;
 }
