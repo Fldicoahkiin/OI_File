fec15279eac13277ccfd4b884e8538390b9acb30
Flacier <Flacier@qq.com>
2021-12-05 22:08:27 +0800

    Update

--- OJ/Yi_Ben_Tong/1325.cpp
+++ OJ/Yi_Ben_Tong/1325.cpp
@@ -1,6 +1,6 @@
 /**
- * 1325������7.4�� ѭ�������ճ̱�
- * �ⷨ:����Ŀû�а�ëǮ��ϵ,���ǵ������ҹ���
+ * 1325：【例7.4】 循环比赛日程表
+ * 解法:跟题目没有半毛钱关系,就是单纯的找规律
  */
 #include <iostream>
 
@@ -14,23 +14,22 @@ int main()
 {
 	int M;
 	cin >>M;
-	int n=1<<M;//��nΪ2��M�η�
-	
+	int n=1<<M;//即n为2的M次方
+
 	matchlist[1][1]=1;
-	
-	int k=1;//k��Ϊ2��i�η�
-	for(int i=1;i<=M;i++)
+
+	int k=1;//k即为2的i次方,通过图像可知这个k相当于一个权值.每次递归要加个k
+	for(int i=1;i<=M;i++)//循环m次来递归
 	{
-		for(int i=1;i<=k;i++)
+		for(int i=1;i<=k;i++)//横向赋值
 		{
 			for(int j=1;j<=k;j++)
 			{
 				matchlist[i][j+k]=matchlist[i][j]+k;
-			}	
+			}
 		}
-			
-		
-		for(int i=1;i<=k;i++)
+
+		for(int i=1;i<=k;i++)//上下镜像翻转赋值
 		{
 			for(int j=1;j<=k;j++)
 			{
@@ -38,11 +37,11 @@ int main()
 				matchlist[i+k][j+k]=matchlist[i][j];
 			}
 		}
-			
+
 //		k=k*2;
 		k=1<<i;
 	}
-	for(int i=1;i<=n;i++)//���
+	for(int i=1;i<=n;i++)//输出
 	{
 		for(int j=1;j<=n;j++)
 		{
---
+++ OJ/Yi_Ben_Tong/1326.cpp
@@ -0,0 +1,33 @@
+﻿/**
+ * 1326：【例7.5】 取余运算（mod）
+ */
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+#
+
+int main()
+{
+	long long b,p,k,ans;
+	cin >>b>>p>>k;
+	cout <<b<<"^"<<p<<" mod "<<k<<"=";
+
+//快速取模
+	long long t=b;
+	ans=1;
+	while(p)
+	{
+		if(p&1)
+		{
+			ans=ans*t%k;
+		}
+		t=t*t%k;
+		p=p>>1;
+	}
+
+
+	cout <<ans;
+	return 0;
+}
