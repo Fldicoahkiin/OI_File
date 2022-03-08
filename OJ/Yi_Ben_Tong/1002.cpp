203dc94e2806552cc4f256afc14811b86fc2aafc
Flacier <Flacier@qq.com>
2022-03-09 00:37:23 +0800

    noob abuse

---
+++ OJ/Yi_Ben_Tong/1002.cpp
@@ -0,0 +1,18 @@
+/**
+ * 1002：输出第二个整数
+ * 
+ * http://ybt.ssoier.cn:8088/problem_show.php?pid=1002
+ */
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main()
+{
+    freopen("1002.in","r",stdin);
+    //freopen("xx.out", "w",stdout);
+    long long readen;
+    cin >>readen;
+    cin >>readen;
+    cout <<readen;
+    return 0;
+}
