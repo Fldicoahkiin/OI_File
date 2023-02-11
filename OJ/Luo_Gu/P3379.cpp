3224a4627d19ba4e2a6c11d44de61e49539dd46a
Flacier <Flacier@qq.com>
2023-02-11 21:09:07 +0800

    Update

--- OJ/Luo_Gu/P3379.cpp
+++ OJ/Luo_Gu/P3379.cpp
@@ -1,3 +1,4 @@
+//Tarjan做法
 #include<bits/stdc++.h>
 using namespace std;
 const int MAXN = 1000000;
@@ -7,16 +8,16 @@ bool vis[MAXN];
 int ans[MAXN];
 int query[MAXN][3];
 
-int head[MAXN];
 struct Node
 {
-	int to,val,nxt;
-} E[MAXN];
+	int to,nxt,head;
+};
+Node E[MAXN];
+Node QE[MAXN];
 
 void add(int num,int u,int v)
 {
 	E[num].to=v;
-	E[num].val=1;
 	E[num].nxt=head[u];
 	head[u]=num++;
 }
@@ -72,3 +73,74 @@ int main()
 	}
 	return 0;
 }
+
+/*
+https://www.luogu.com.cn/discuss/532906
+#include <iostream>
+using namespace std;
+namespace Answer {
+    constexpr int MAXN(500005);
+    int n, m, s,
+        eto[MAXN << 1], enext[MAXN << 1], ehead[MAXN], par[MAXN],
+        qto[MAXN << 1], qnext[MAXN << 1], qhead[MAXN], ans[MAXN];
+    inline void newEdge(int src, int des) {
+        static int cnt(1);
+        ++ cnt;
+        eto[cnt] = des;
+        enext[cnt] = ehead[src];
+        ehead[src] = cnt;
+        ++ cnt;
+        eto[cnt] = src;
+        enext[cnt] = ehead[des];
+        ehead[des] = cnt;
+        return;
+    }
+    inline void newQuery(int src, int des) {
+        static int cnt(1);
+        ++ cnt;
+        qto[cnt] = des;
+        qnext[cnt] = qhead[src];
+        qhead[src] = cnt;
+        ++ cnt;
+        qto[cnt] = src;
+        qnext[cnt] = qhead[des];
+        qhead[des] = cnt;
+        return;
+    }
+    int findPar(const int x) {
+        return (par[x] == x) ? (x) : (par[x] = findPar(par[x]));
+    }
+    void tarjan(int vert, int from) {
+        par[vert] = vert;
+        for (int i(ehead[vert]); i; i = enext[i])
+            if (eto[i] != from)
+                tarjan(eto[i], vert);
+        for (int i(qhead[vert]); i; i = qnext[i])
+            if (! ans[i >> 1])
+                ans[i >> 1] = findPar(qto[i]);
+        par[vert] = from;
+        return;
+    }
+    void main() {
+        scanf("%d%d%d", &n, &m, &s);
+        for (int i(1); i < n; ++ i) {
+            static int x, y;
+            scanf("%d%d", &x, &y);
+            newEdge(x, y);
+        }
+        for (int i(0); i < m; ++ i) {
+            static int x, y;
+            scanf("%d%d", &x, &y);
+            newQuery(x, y);
+        }
+        tarjan(s, s);
+        for (int i(1); i <= m; ++ i)
+            printf("%d\n", ans[i]);
+        return;
+    }
+}
+int main() {
+    Answer::main();
+    return 0;
+}
+*/
---
+++ OJ/Luo_Gu/P3379_2.cpp
@@ -0,0 +1,67 @@
+//倍增做法
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+const int MAXN = 1000000;
+
+int lg[MAXN];
+int depth[MAXN];
+int fa[MAXN][233];
+struct Node
+{
+	int to,nxt,head;
+}E[MAXN];
+
+int cnt;
+void add(int u,int v)
+{
+	E[cnt].to=to;
+	E[cnt].nxt=nxt;
+	E[u].head=cnt++;
+}
+
+void LgDefine()
+{
+	lg[1]=0;
+	
+	for(int i=2;i<=MAXN;i++)
+	{
+		lg[i]=lg[i/2]+1;
+	}
+}
+
+void DFS(int u,int f)
+{
+	depth[u]=depth[a]+1;
+	for(int i=1;i<=lg[depth[u]];i++)
+	{
+		
+	}
+}
+
+int LCA(int x,int y)
+{
+	
+}
+
+int main()
+{
+	int N,M,S;
+	cin>>N>>M>>S;
+	for(int i=1;i<=N-1;i++)
+	{
+		int x,y;
+		cin>>x>>y;
+		add(x,y);
+		add(y,x);
+	}
+	LgDefine();
+	DFS(S,0);
+	while(M--)
+	{
+		int x,y;
+		cin>>x>>y;
+		cout<<LCA(x,y)<<endl;
+	}
+	return 0;
+}
