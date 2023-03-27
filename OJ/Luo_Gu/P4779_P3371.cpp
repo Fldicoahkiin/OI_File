aae3fa9091e8db8ecc7bbc1f8b297aa188e35490
Flacier <flacier@qq.com>
2023-03-27 22:10:53 +0800

    Update 0AC

--- OJ/Luo_Gu/P4779_P3371.cpp
+++ OJ/Luo_Gu/P4779_P3371.cpp
@@ -1,16 +1,17 @@
-#include<bits/stdc++>
+#include<bits/stdc++.h>
 #define ll long long
 using namespace std;
-const int MAXN = 100000;
+
+const int MAXN = 500001;
 
 int head[MAXN];
 struct
 {
-	int to,nxt,val;
+	ll to,nxt,val;
 }E[MAXN];
 
 int cnt;
-void add(int u,int v,int val)
+inline void Eupdate(int u,int v,int val)
 {
 	cnt++;
 	E[cnt].to=u;
@@ -19,9 +20,35 @@ void add(int u,int v,int val)
 	head[u]=cnt;
 }
 
-void dfs(int s,int i)
+ll dis[MAXN];
+bool vis[MAXN];
+void dijkstra(int u,int n)
 {
+	ll INF=2147483647;
+	for(int i=1;i<=n;i++) dis[i]=INF;
+	dis[u]=0;
 	
+	for(int i=1;i<=n;i++)
+	{
+		// for(int j=1;j<=n;j++)
+		// {
+			// if(vis[j]==0 && dis[j]<INF)
+			// {
+				// INF=dis[j];
+				// u=j;
+			// }
+		// }
+		
+		vis[u]=1;
+		for(int i=head[u];i!=0;i=E[i].nxt)
+		{
+			if(vis[E[i].to]==0)
+			{
+				dis[E[i].to]=min(dis[E[i].to],dis[u]+E[i].val);
+			}
+		}
+	}
+	return;
 }
 
 int main()
@@ -30,14 +57,19 @@ int main()
 	cin>>n>>m>>s;
 	for(int i=1;i<=m;i++)
 	{
-		int u,v,val;
+		ll u,v,val;
 		cin>>u>>v>>val;
-		add(u,v,val);
+		Eupdate(u,v,val);
 	}
+	
+	dijkstra(s,n);
 	
 	for(int i=1;i<=n;i++)
 	{
-		dfs(s,i);
+		cout<<dis[i]<<' ';
 	}
 	return 0;
 }
+
+//链式前向星
+//https://www.luogu.com.cn/blog/_post/115781
